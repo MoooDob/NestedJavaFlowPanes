b391d1b989ad83f3c1fd3e823d968e5de727ff16
Marc O. Rüdel <mor@uni-bremen.de>
2019-12-07 17:16:54 +0100

    V 0.2 random structure created

--- NestedFlowPanes/src/FlowPaneExample.java
+++ NestedFlowPanes/src/FlowPaneExample.java
@@ -1,6 +1,4 @@
-import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
@@ -15,13 +13,20 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
          
 public class FlowPaneExample extends Application { 
+	
+   int maxNestingDepth = 3;
+   int maxNumberOfPanes = 30;
+   int maxNumberOfChilds = 3;
+   
+   int seed = 1534;	   
+   Random randomizer; 
+	   
    @Override 
-   public void start(Stage stage) {      
+   public void start(Stage stage) {
 	   
+	  randomizer = new Random(seed);
 	   
-	   FlowPane subflowPane = createBlock(5, 120, new Color(67, 35, 85), 1233);
-	   FlowPane flowPane = createBlock(10, 300, new Color(103, 201, 167), 1231, subflowPane);
-
+	  Pane flowPane = createTree(0);	  
       
       // ScrollPane
       ScrollPane scrollPane = new ScrollPane();
@@ -43,29 +48,58 @@ public class FlowPaneExample extends Application {
       //Displaying the contents of the stage 
       stage.show(); 
 
-//      System.out.println("flowPane.Width: " + flowPane.getWidth());
-
    }
-private FlowPane createBlock(int numItems, 
-		int maxHeight, 
-		Color backgroundColor, 
-		int randomseed,
-		Pane ... subPanes
+private Pane createTree( 
+		int level
 		) {
-	    
-       Random random = new Random(randomseed);
+	
+	int numOfChilds = randomizer.nextInt(this.maxNumberOfChilds);
+	Pane[] childPanes;
+	
+	if (level < maxNestingDepth) {
+		
+		childPanes = new Pane[numOfChilds];
+				
+		for (int idx = 0; idx < numOfChilds; idx++) {
+			childPanes[idx] = createTree(level++);
+		}
+		
+	} else childPanes = new Pane[0];
+	
+	Pane pane = createBlock(childPanes);
+	   	   
+	return pane;
+}
+
+
+private FlowPane createBlock(Pane ... subPanes) {
+       
+	   float transformFactor = 1.0f;
        
 	   double maxItemHeight = 0;
+	   int totalArea = 0;
 	   
-	   ArrayList<Pane> items = new ArrayList<Pane>();
-	   Collections.addAll(items, subPanes);
+	   // add subPanes to list
+	   ArrayList<Pane> panes = new ArrayList<Pane>();
+	   for (Pane p : subPanes) {
+		   panes.add(p);
+		   if (p == null) {
+			   totalArea = totalArea;
+		   }
+		   maxItemHeight = Math.max(maxItemHeight, p.getHeight());
+		   totalArea += p.getHeight() * p.getWidth();
+	   }
+//	   Collections.addAll(items, subPanes);
+	   
+	   int numOfPanes = randomizer.nextInt(this.maxNumberOfPanes);
 
-	   for(int idx=0; idx<=numItems; idx++) {
-		   double height = random.nextInt(100);
-		   double width = random.nextInt(70);
-		   maxItemHeight = Math.max(maxItemHeight, height);
+	   for(int idx=0; idx<numOfPanes; idx++) {
+		   double paneHeight = randomizer.nextInt(100);
+		   double paneWidth = randomizer.nextInt(70);
+		   maxItemHeight = Math.max(maxItemHeight, paneHeight);
+		   totalArea += paneHeight * paneWidth;
 		   Pane newPane = new Pane();
-		   items.add(newPane);
+		   panes.add(newPane);
 		   Label newLabel = new Label("" + (idx + 1));
 		   
 		   // centering Text in Pane: https://stackoverflow.com/questions/36854031/how-to-center-a-label-on-a-pane-in-javafx
@@ -73,39 +107,37 @@ private FlowPane createBlock(int numItems,
 		   newLabel.layoutYProperty().bind(newPane.heightProperty().subtract(newLabel.heightProperty()).divide(2));
 		   
 		   newPane.getChildren().add(newLabel);
-		   newPane.setPrefSize(width, height);
-		   newPane.setStyle("-fx-background-color: rgba(" + random.nextInt(255)+ ", " + random.nextInt(255) + ", " + random.nextInt(255) + ", 0.5); -fx-background-radius: 10;");
+		   newPane.setPrefSize(paneWidth, paneHeight);
+		   newPane.setStyle("-fx-background-color: rgba(" + randomizer.nextInt(255) + ", " + randomizer.nextInt(255) + ", " + randomizer.nextInt(255) + ", 0.5); -fx-background-radius: 10;");
 	   }
+	   
+	   // height of squared total area 
+	   int maxHeight = (int) (Math.sqrt(totalArea) * transformFactor); 
       
       //Creating a Flow Pane 
       FlowPane flowPane = new FlowPane(Orientation.VERTICAL);    
        
-      //Setting the horizontal gap between the nodes 
+      //Setting the horizontal and vertical gap between the nodes 
       flowPane.setHgap(0);
       flowPane.setVgap(0);
       
-//     flowPane.setMaxHeight(Math.max(maxHeight, maxItemHeight) + 6 /*Insets*/);
-//      flowPane.setMinHeight(Math.max(maxHeight, maxItemHeight) + 6 /*Insets*/);
-//      flowPane.setPrefHeight(Math.max(maxHeight, maxItemHeight) + 6 /*Insets*/);
-      
+      //Setting the 
       flowPane.setPrefWrapLength(Math.max(maxHeight, maxItemHeight) + 6 /*Insets*/);
       
       flowPane.autosize();
-      
-//      System.out.println("flowPane.Width: " + flowPane.getWidth());
-      
-      flowPane.setStyle("-fx-background-color: rgba(" + backgroundColor.getRed() + ", " + backgroundColor.getGreen() + ", " + backgroundColor.getBlue() + ", " + backgroundColor.getAlpha() + "); -fx-background-radius: 10;");
+          
+      //flowPane.setStyle("-fx-background-color: rgba(" + randomizer.nextInt(255) + ", " + randomizer.nextInt(255) + ", " + randomizer.nextInt(255) + ", " + randomizer.nextFloat() + "); -fx-background-radius: 10;");
+      flowPane.setStyle("-fx-background-color: rgba(" + 255 + ", " + 255 + ", " + 255 + ", " + 0 + "); -fx-background-radius: 10; -fx-border-color: black");
            
       //Retrieving the observable list of the flow Pane 
       ObservableList<Node> list = flowPane.getChildren(); 
       
       //Adding all the nodes to the flow pane and add margins 
-      for(int idx=0; idx<numItems; idx++) {
-    	  list.add(items.get(idx)); 
-    	  FlowPane.setMargin(items.get(idx), new Insets(3, 3, 3, 3));
+      for(Pane p : panes) {
+    	  list.add(p); 
+    	  FlowPane.setMargin(p, new Insets(3, 3, 3, 3));
 	   }
       
-//      System.out.println("flowPane.Width: " + flowPane.getWidth());
 	return flowPane;
 } 
    public static void main(String args[]){
